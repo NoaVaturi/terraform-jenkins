1dedb59404d741d9bce433bf8f70dd7835282ecc
NoaVaturi <vaturinoah@gmail.com>
2024-12-19 17:00:34 +0200

    added full path to requirements.txt

--- app/steps.sh
+++ app/steps.sh
@@ -8,8 +8,10 @@ cd app
 python3 -m venv env  
 source env/bin/activate  
 
+echo "Current directory: $(pwd)"
+
 # Install dependencies from requirements.txt for testing
-pip install --cache-dir=/var/lib/jenkins/.cache/pip -r app/requirements.txt
+pip install --cache-dir=/var/lib/jenkins/.cache/pip -r $(pwd)/app/requirements.txt
 python -m pip install --upgrade pip
 
 # Deactivate the virtual environment after the tests are complete
