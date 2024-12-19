8feb242121e9256d96b061f1bad69a4581ac2c51
NoaVaturi <vaturinoah@gmail.com>
2024-12-19 18:33:05 +0200

    updated virtual enviroment for jenkins

--- app/steps.sh
+++ app/steps.sh
@@ -2,16 +2,27 @@
 #!/bin/bash
 
 # Create and activate a virtual environment for testing in Jenkins
-mkdir -p app
-cd app
+mkdir -p env
+cd env
 
-python3 -m venv env  
+if [ ! -d "env" ]; then
+    python3 -m venv env
+else
+    echo "Virtual environment already exists."    
+fi
+
 source env/bin/activate  
 
 echo "Current directory: $(pwd)"
 
-# Install dependencies from requirements.txt for testing
-pip install --cache-dir=/var/lib/jenkins/.cache/pip -r requirements.txt -v
+if [ -f "../app/requirements.txt" ]; then
+    echo "Found requirements.txt, installing dependencies..."
+    pip install --cache-dir=/var/lib/jenkins/.cache/pip -r ../app/requirements.txt -v
+else
+    echo "requirements.txt not found!"
+    exit 1
+fi
+
 python -m pip install --upgrade pip
 
 # Deactivate the virtual environment after the tests are complete
