2caf0a485c5c332be9f4e9b72cbf2048e063abfa
NoaVaturi <vaturinoah@gmail.com>
2024-12-20 13:02:16 +0200

    changes to steps.sh

--- app/steps.sh
+++ app/steps.sh
@@ -1,49 +1,18 @@
 
 #!/bin/bash
 
-set -xe
+mkdir -p env_app
+cd env_app
 
-cd "$(dirname "$0")/.." 
-echo "Changed to project root directory: $(pwd)"
-
-
-echo "Removing existing virtual environment."
-rm -rf env
-
-
-echo "Cleaning up .pyc files."
-find . -name "*.pyc" -exec rm -f {} \;
-
-
-echo "Creating virtual environment."
-python3 -m venv env
-
-
-echo "Using Python from: $(which python3)"
-echo "Python version: $(python3 --version)"
-
-
-echo "Activating virtual environment."
+python3 -m venv env  
 source env/bin/activate  
 
-
-python3 -m pip install --upgrade pip
-
-
-echo "Environment activated. Using Python from: $(which python3)"
-echo "Current Python version: $(python3 --version)"
+python3 -m pip install --upgrade pip && pip install -r ../requirements.txt -v
 
 
-pip install -r app/requirements.txt -v
-
-
-echo "Deactivating virtual environment."
 if [ -n "$VIRTUAL_ENV" ]; then
     deactivate
     echo "Virtual environment deactivated."
 else
     echo "Virtual environment was not activated."
 fi
-
-
-echo "Virtual environment deactivated."
