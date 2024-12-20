862b1d10418296c91a1b3315f4e5b09bdebb8d08
NoaVaturi <vaturinoah@gmail.com>
2024-12-20 15:45:41 +0200

    fixed path

--- steps.sh
+++ steps.sh
@@ -9,7 +9,7 @@ python3 -m venv env
 source env/bin/activate  
 
 # Install dependencies from requirements.txt for testing
-python3 -m pip install --upgrade pip && pip install --cache-dir=/var/lib/jenkins/.cache/pip -r a../requirements.txt -v
+python3 -m pip install --upgrade pip && pip install --cache-dir=/var/lib/jenkins/.cache/pip -r ../requirements.txt -v
 
 # Deactivate the virtual environment after the tests are complete
 deactivate
